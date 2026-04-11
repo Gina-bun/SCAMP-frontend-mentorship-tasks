b2964f0339d21883f00dbf9b731113cbdf4393d0
Gina-bun <64642337+Gina-bun@users.noreply.github.com>
2026-04-11 18:18:27 +0000

    style: add colours and layout styles

--- Result-Summary-Component/src/SummaryCard.jsx
+++ Result-Summary-Component/src/SummaryCard.jsx
@@ -4,50 +4,68 @@ export function SummaryCard({data}){
 // - Orangey yellow: hsl(39, 100%, 56%)
 // - Green teal: hsl(166, 100%, 37%)
 // - Cobalt blue: hsl(234, 85%, 45%)
-    const colours = {
+    const bgColours = {
         Reaction: "hsla(0, 100%, 67%, 0.06)",
         Memory: "hsla(39, 100%, 56%, 0.06)",
         Verbal: "hsla(166, 100%, 37%, 0.06)",
         Visual: "hsla(234, 85%, 45%, 0.06)",
     }
 
+    const textColours = {
+        Reaction: "hsl(0, 100%, 67%)",
+        Memory: "hsl(39, 100%, 56%)",
+        Verbal: "hsl(166, 100%, 37%)",
+        Visual: "hsl(234, 85%, 45%)",
+    }
+
     return (
         <>
           <div className="container">
-            <div className="result flex flex-col gap-5 items-center text-center py-8 px-12 border-b rounded-bl-3xl rounded-br-3xl">
-                <h3>Your Result</h3>
+            <div 
+            className="result text-white flex flex-col gap-4 items-center text-center
+             py-3 px-12 rounded-bl-3xl rounded-br-3xl"
+             style={{background: "linear-gradient( to bottom , hsl(252, 100%, 67%), hsl(241, 81%, 54%))"}}
+             >
+                <h3 className="font-bold text-gray-200">Your Result</h3>
 
-                <div className="average rounded-full border p-4">
-                        <p>76</p>
+                <div 
+                className="average rounded-[100%] py-3 px-5"
+                style={{background: "linear-gradient(to bottom, hsla(256, 72%, 46%, 1),hsla(241, 72%, 46%, 0))"}}
+                >
+                        <p className="text-6xl">76</p>
                         <p>of 100</p>
                 </div>
 
-                <p>Great</p>
+                <p className="text-2xl font-bold mb-[-8px]">Great</p>
                 <p>You scored higher than 65% of the people who have taken these tests</p>
             </div>
 
-            <div className="summary py-5 px-7">
-                <h3>Summary</h3>
+            <div className="summary py-1 px-7">
+                <h3 className=" text-gray-700 py-4 font-bold">Summary</h3>
                 {/* list content here */}
                 <div className="summary-items flex flex-col gap-y-5">
                     {
                          data.map((dataObj) => {
                     return  <div 
                             className="flex justify-between py-3 rounded-md px-3" 
-                            style={{backgroundColor: colours[dataObj.category]}} 
+                            style={{backgroundColor: bgColours[dataObj.category]}} 
                             key={dataObj.category}>
 
                                 <div className={`flex gap-1.5`} >
                                    <img className="" src={dataObj.icon} alt={`${dataObj.category} icon`} />
-                                   <p> {dataObj.category}</p>
+                                   <p style={{color: textColours[dataObj.category]}}> {dataObj.category}</p>
                                 </div>
-                                <p>{dataObj.score} / 100</p>
+                                <p>{dataObj.score} <span className="text-gray-500">/ 100</span></p>
                             </div>
                             
                         })
                     }
                 </div>
-                <button className="border rounded-3xl p-2 w-[80vw] mt-5">Continue</button>
+                <button 
+                className="border rounded-3xl py-3 w-full mt-5 text-white"
+                style={{backgroundColor: "hsl(224, 30%, 27%)"}}
+                >
+                    Continue</button>
             </div>
           </div>
         </>
