4e6eb2bef3ed14b51521b87ff24c9407beac795d
Gina-bun <64642337+Gina-bun@users.noreply.github.com>
2026-04-11 17:46:16 +0000

    feat: add summary section

--- Result-Summary-Component/src/App.jsx
+++ Result-Summary-Component/src/App.jsx
@@ -1,11 +1,12 @@
-import { SummaryCard } from './SummaryCard'
+import { SummaryCard } from './SummaryCard.jsx'
+import data from "./data.json"
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <SummaryCard/>
+      <SummaryCard data={data}/>
     </>
   )
 }
--- Result-Summary-Component/src/SummaryCard.jsx
+++ Result-Summary-Component/src/SummaryCard.jsx
@@ -1,4 +1,16 @@
-export function SummaryCard(){
+export function SummaryCard({data}){
+
+// - Light red: hsl(0, 100%, 67%)
+// - Orangey yellow: hsl(39, 100%, 56%)
+// - Green teal: hsl(166, 100%, 37%)
+// - Cobalt blue: hsl(234, 85%, 45%)
+    const colours = {
+        Reaction: "hsla(0, 100%, 67%, 0.06)",
+        Memory: "hsla(39, 100%, 56%, 0.06)",
+        Verbal: "hsla(166, 100%, 37%, 0.06)",
+        Visual: "hsla(234, 85%, 45%, 0.06)",
+    }
+
     return (
         <>
           <div className="container">
@@ -14,10 +26,28 @@ export function SummaryCard(){
                 <p>You scored higher than 65% of the people who have taken these tests</p>
             </div>
 
-            <div className="summary py-5 px-8">
+            <div className="summary py-5 px-7">
                 <h3>Summary</h3>
                 {/* list content here */}
-                <button className="border rounded-3xl p-2 w-[80vw]">Continue</button>
+                <div className="summary-items flex flex-col gap-y-5">
+                    {
+                         data.map((dataObj) => {
+                    return  <div 
+                            className="flex justify-between py-3 rounded-md px-3" 
+                            style={{backgroundColor: colours[dataObj.category]}} 
+                            key={dataObj.category}>
+
+                                <div className={`flex gap-1.5`} >
+                                   <img className="" src={dataObj.icon} alt={`${dataObj.category} icon`} />
+                                   <p> {dataObj.category}</p>
+                                </div>
+                                <p>{dataObj.score} / 100</p>
+                            </div>
+                            
+                        })
+                    }
+                </div>
+                <button className="border rounded-3xl p-2 w-[80vw] mt-5">Continue</button>
             </div>
           </div>
         </>
