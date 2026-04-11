e37b740076576b7c856aebc24fd2e6204e2fd1db
Gina-bun <64642337+Gina-bun@users.noreply.github.com>
2026-04-11 19:04:25 +0000

    style: add final styles

--- Result-Summary-Component/src/SummaryCard.jsx
+++ Result-Summary-Component/src/SummaryCard.jsx
@@ -22,22 +22,22 @@ export function SummaryCard({data}){
         <>
           <div className="container">
             <div 
-            className="result text-white flex flex-col gap-4 items-center text-center
+            className="result text-white flex flex-col gap-3 items-center text-center
              py-3 px-12 rounded-bl-3xl rounded-br-3xl"
              style={{background: "linear-gradient( to bottom , hsl(252, 100%, 67%), hsl(241, 81%, 54%))"}}
              >
                 <h3 className="font-bold text-gray-200">Your Result</h3>
 
                 <div 
-                className="average rounded-[100%] py-3 px-5"
+                className="average rounded-[100%] py-4.5 px-6.5"
                 style={{background: "linear-gradient(to bottom, hsla(256, 72%, 46%, 1),hsla(241, 72%, 46%, 0))"}}
                 >
-                        <p className="text-6xl">76</p>
-                        <p>of 100</p>
+                        <p className="text-6xl font-extrabold">76</p>
+                        <p className="font-bold" style={{color: "hsl(241, 100%, 89%)"}}>of 100</p>
                 </div>
 
-                <p className="text-2xl font-bold mb-[-8px]">Great</p>
-                <p>You scored higher than 65% of the people who have taken these tests</p>
+                <p className="text-2xl font-bold mb-[-8px]" >Great</p>
+                <p className="text-[18px]" style={{color: "hsl(221, 100%, 96%)"}}>You scored higher than 65% of the people who have taken these tests</p>
             </div>
 
             <div className="summary py-1 px-7">
@@ -47,7 +47,7 @@ export function SummaryCard({data}){
                     {
                          data.map((dataObj) => {
                     return  <div 
-                            className="flex justify-between py-3 rounded-md px-3" 
+                            className="flex justify-between py-3 rounded-lg px-3" 
                             style={{backgroundColor: bgColours[dataObj.category]}} 
                             key={dataObj.category}>
 
